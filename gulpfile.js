9b9c19ff76bb3bb67fc6e1ea74b62b9d33b8ee1c
KaitlynDodds <kadodds.dev@kadodds.com>
2018-09-06 12:00:23 -0600

    styles and scripts tasks functioning as specified, source maps also generated for styles and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const useref        = require('gulp-useref');
 const gulpif        = require('gulp-if');
 const sass          = require('gulp-sass');
 const nano          = require('gulp-cssnano');
+const maps          = require('gulp-sourcemaps');
+const concat        = require('gulp-concat');
+const rename        = require('gulp-rename');
 const del           = require('del');
 
 const options = {
@@ -19,17 +22,44 @@ gulp.task('html', function() {
             .pipe(gulp.dest(options.dist));
 });
 
+gulp.task('concatScripts', function() {
+    return gulp.src(options.src + 'js/**/*.js')
+            .pipe(maps.init())
+            .pipe(concat('global.js'))
+            .pipe(maps.write('./'))
+            .pipe(gulp.dest(options.src + 'js'))
+});
+
+gulp.task('minifyScripts', function() {
+    return gulp.src(options.src + 'js/global.js')
+            .pipe(uglify())
+            .pipe(rename('all.min.js'))
+            .pipe(gulp.dest(options.dist + 'styles'))
+});
+
 gulp.task('compileSass', function() {
     return gulp.src(options.src + 'sass/global.scss')
+            .pipe(maps.init())
             .pipe(sass())
+            .pipe(maps.write('./'))
             .pipe(gulp.dest(options.src + 'css'));
 });
 
-gulp.task('styles', gulp.series('compileSass', 'html'));
+gulp.task('minifyStyles', function() {
+    return gulp.src(options.src + 'css/global.css')
+            .pipe(nano())
+            .pipe(rename('all.min.css'))
+            .pipe(gulp.dest(options.dist + 'styles'))
+});
 
-gulp.task('scripts', gulp.series('html'));
+gulp.task('styles', gulp.series('compileSass', 'minifyStyles'));
+gulp.task('scripts', gulp.series('concatScripts', 'minifyScripts'));
 
 gulp.task('clean', function() {
-    return del(options.dist);
+    return del([
+        options.dist,
+        options.src + 'js/global.js*',
+        options.src + 'css'
+    ]);
 });
 
